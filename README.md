8070c2f5c6661c84f45e6d24809454b6971b6685
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-08 09:27:39 +0900

    3.0
    import express from "express"
    app.listen

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 dependencies -> 프로젝트 실행을 위해 필요한 dependencies 즉, 프로젝트에 필요한 dependencies
+
 devDependencies -> 개발자에게 필요한 dependencies
+
+app.listen(포트번호, callback)
--- index.js
+++
@@ -1,7 +0,0 @@
-// express import
-// const express = require("express"); === import express from "express"
-import express from "express";
-
-const app = express();
-
-console.log("키킥");
