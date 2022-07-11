f4f50abbc35b87f952393db814fa5d673344af85
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-11 21:12:14 +0900

    3.3
    요청에 대한 응답

--- src/server.js
+++ src/server.js
@@ -6,11 +6,18 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome =  () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login Here");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
 // app.listen(포트번호, callback)
