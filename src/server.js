01b3eb935bc2e30ba538100f1023cd1f989409f1
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-13 14:03:48 +0900

    5.2+

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
-console.log(process.cwd());
+console.log(process.cwd()); // precess.cwd() -> 현재 작업중인 디렉토리 확인
+// cwd == node.js가 실행되는 위치 == package.json 위치
 
 const app = express();
 const logger = morgan("dev");
