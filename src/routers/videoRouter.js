4511986a08dbfb0d67fb5796864bbfa3e952244f
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-13 13:05:28 +0900

    4.8
    URL parameters part two
    regular Expression

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,10 +2,10 @@ import express from "express";
 import {see, edit, upload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
-
+// (\\d+) -> regular Expression -> 숫자만 가능하게 설정
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
