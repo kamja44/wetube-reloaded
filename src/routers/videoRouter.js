844beab48a4617fbff0943b4ae1c5c40f0ef190f
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-15 14:55:50 +0900

    6.3
    Edit Video Part Two
    form의 data는 req.body로 받는다

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,8 +6,9 @@ const videoRouter = express.Router();
 // (\\d+) -> regular Expression -> 숫자만 가능하게 설정
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); // get과 post를 동시에 사용
+// videoRouter.get("/:id(\\d+)/edit", getEdit);
+// videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug"); // view engine을 pug로 설정하여 express에 명시한다.
 app.set("views",process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({extended : true})); // express가 form의 값을 읽을 수 있게 설정
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
