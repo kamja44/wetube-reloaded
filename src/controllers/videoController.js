7ccc6b370468a3ff2e972245ba2e0fc233a8764c
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-07-26 10:39:28 +0900

    6.21 Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,6 +7,7 @@ export const home = async(req, res) => {
     const videos = await Video.find({});
     return res.render("home", {pageTitle : "Home", videos}); 
 }
+
 export const watch = async(req, res) => {
     // const id = req.params.id; 코드와 const {id} = req.params; 코드는 동일하다.
     const { id } = req.params;
@@ -16,6 +17,7 @@ export const watch = async(req, res) => {
     }
     return res.render("watch", {pageTitle : video.title, video });
 }
+
 export const getEdit = async (req, res) =>{
     const {id} = req.params;
     const video = await Video.findById(id);
@@ -24,15 +26,26 @@ export const getEdit = async (req, res) =>{
     }
     return res.render("edit",{pageTitle : `Edit : ${video.title} `,video});
 } 
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
     const {id} = req.params;
-    // console.log(req.body);
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
+    console.log(req.body);
+    const video = await Video.findById(id);
+    if(video === null){
+        return res.render("404", {pageTitle : "Video not Found."});
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`);
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle : "Upload Video"});
 };
+
 export const postUpload = async (req, res) => {
     const {title, description, hashtags} = req.body;
     try{
