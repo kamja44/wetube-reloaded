a72477a73e579b081f1b73485be38eed13bec662
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-04 11:10:24 +0900

    6.25
    Delete Video
    Recap Router, Controller

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,6 +7,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit); // get과 post를 동시에 사용
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
