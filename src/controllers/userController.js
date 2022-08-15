597c64e54c9852870666270cf80918a0d89d5de5
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-15 15:16:14 +0900

    7.3
    Form Validation
    check password, unique username, unique email

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,42 @@
 import User from "../models/User";
-export const getJoin = (req, res) => res.render("join",{pageTitle : "join" });
-export const postJoin = async(req, res) => {
-    const {name, username, email, password, location } = req.body;
-    await User.create({
-        name, username, email, password, location,
+export const getJoin = (req, res) => res.render("join", { pageTitle: "join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
     });
-    /*
+  }
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  //   const emailExists = await User.exists({ email });
+  //   if (emailExists) {
+  //     return res.render("join", {
+  //       pageTitle,
+  //       errorMessage: "This email is already taken.",
+  //     });
+  //   };
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  /*
     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash){})
     myplaintextPassword <- 사용자의 비밀번호(해시함수를 적용시킬 비밀번호)
     saltRounds <- 해시함수를 얼마나 적용시킬것인가 즉, 몇 번 해싱할것인지 설정
     function(err, hash){} <- 출력값(결과값)
     */
-    return res.redirect("/login");
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove");
