f37ddeab836ba6d4b879c3bb69d506b571062e3c
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-16 13:37:35 +0900

    7.7
    npm i express-session
    Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 // const express = require("express"); === import express from "express"
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -17,6 +18,21 @@ app.set("view engine", "pug"); // view engine을 pug로 설정하여 express에 
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // express가 form의 값을 읽을 수 있게 설정
+// session은 Router 위에 정의해야한다.
+app.use(session({
+    secret : "Hello",
+    resave : true,
+    saveUninitialized : true,
+})
+);
+app.use((req, res, next) =>{
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    })
+    
+})
+
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
