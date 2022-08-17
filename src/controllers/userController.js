18e4519111663d4cf98e01e2cc0b0acc4de312a0
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-17 17:23:45 +0900

    7.18
    Github Login part Three

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -75,7 +75,7 @@ export const postLogin = async(req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id : "49f81e4d2392c1420835",
+    client_id : process.env.GH_CLIENT,
     allow_signup : false,
     scope : "read:user user:email", // seperated by space
   };
@@ -84,8 +84,24 @@ export const startGithubLogin = (req, res) => {
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
-export const finishGithubLogin = (req, res) => {
-
+export const finishGithubLogin = async(req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id : process.env.GH_CLIENT,
+    client_secret : process.env.GH_SECRET,
+    code : req.query.code,
+  }
+  console.log("req.body", req.body);
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method : "POST",
+    headers :{
+      Accept : "application/json",
+    }
+  });
+  const json = await data.json();
+  console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit");
