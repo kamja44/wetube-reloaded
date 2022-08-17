5cf4ec8e5b7638154d96534ee31157a5d804c420
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-18 02:55:59 +0900

    7.20
    Github Login part Five

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,7 @@ export const postJoin = async (req, res) => {
   //       errorMessage: "This email is already taken.",
   //     });
   //   };
-  try{
+  try {
     await User.create({
       name,
       username,
@@ -34,11 +34,11 @@ export const postJoin = async (req, res) => {
       location,
     });
     return res.redirect("/login");
-  }catch(error){
-    return res.sattus(400).render("join",{
-      pageTitle : "Join",
-      errorMessage : error._message,
-    })
+  } catch (error) {
+    return res.sattus(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: error._message,
+    });
   }
   /*
     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash){})
@@ -47,24 +47,24 @@ export const postJoin = async (req, res) => {
     function(err, hash){} <- 출력값(결과값)
     */
 };
-export const getLogin = (req, res) => 
-  res.render("login", {pageTitle : "Login"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
-export const postLogin = async(req, res) => {
-  const {username, password} = req.body;
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({username});
-  if(!user){
+  const user = await User.findOne({ username });
+  if (!user) {
     return res.status(400).render("login", {
       pageTitle,
-      errorMessage : "An account with this username does not exists.",
+      errorMessage: "An account with this username does not exists.",
     });
   }
   const ok = await bcrypt.compare(password, user.password);
-  if(!ok){
-    return res.status(400).render("login",{
+  if (!ok) {
+    return res.status(400).render("login", {
       pageTitle,
-      errorMessage : "Wrong password",
+      errorMessage: "Wrong password",
     });
   }
   // session initialize(세션 초기화)
@@ -76,9 +76,9 @@ export const postLogin = async(req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id : process.env.GH_CLIENT,
-    allow_signup : false,
-    scope : "read:user user:email", // seperated by space
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email", // seperated by space
   };
   const params = new URLSearchParams(config).toString(); // config Object를 URL형식으로 반한다.
   // 이런식으로 client_id=49f81e4d2392c1420835&allow_signup=false&scope=read%3Auser+user%3Aemail
@@ -88,31 +88,76 @@ export const startGithubLogin = (req, res) => {
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
-    client_id : process.env.GH_CLIENT,
-    client_secret : process.env.GH_SECRET,
-    code : req.query.code,
-  }
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+
+  // aysnc await이 아닌 promise then을 사용해서 userData 가져오는 방법
+  // fetch(finalUrl, {
+  //   method : "POST",
+  //   headers:{
+  //     Accept : "application/json",
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((json) => {
+  //     if("access_token" in json){
+  //       const {access_token} = tokenRequest;
+  //       const apiUrl = "https://api.github.com";
+  //       fetch(`${apiUrl}/user`, {
+  //         headers:{
+  //           Authorization:`token ${access_token}`,
+  //         },
+  //       })
+  //         .then((response) => response.json())
+  //         .then((json) => {
+  //           fetch(`${apiUrl}/user/emails`,{
+  //             headers:{
+  //               Authorization : `token ${access_token}`,
+  //             },
+  //           });
+  //         });
+  //     }
+  //   });
+
   const tokenRequest = await (
     await fetch(finalUrl, {
-    method : "POST",
-    headers :{
-      Accept : "application/json",
-    },
-  })
-  ).json();
-  if("access_token" in tokenRequest){
-    // access api
-    const {access_token} = tokenRequest; // json변수의 access_token 추출
-    const userRequest = await (
-      await fetch("https://api.github.com/user",{
-      headers:{
-        Authorization : `token ${access_token}`,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
       },
-    })).json();
-    console.log(userRequest);
-  }else{
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    // access api
+    const { access_token } = tokenRequest; // json변수의 access_token 추출
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    // console.log(emailData);
+    const email = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!email) {
+      return res.redirect("/login");
+    }
+  } else {
     return res.redirect("/login");
   }
 };
