99783ee2f8332f8e2fcb98182b676f3e895dea75
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-18 11:05:52 +0900

    8.1
    Protector and Public Middlewares

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,17 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
-
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware
+} from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware ,logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+// all() <- 모든 http method에 적용할 때 사용한다.
+userRouter.get("/github/start", publicOnlyMiddleware ,startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware ,finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
