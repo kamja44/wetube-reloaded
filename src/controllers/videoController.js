760151d957bf03254b444944822b384035a2825e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-22 16:48:30 +0900

    8.13
    User's Videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -76,7 +76,7 @@ export const postUpload = async (req, res) => {
   //   },
   // } = req;
   try {
-    await Video.create({
+    const newVideo = await Video.create({
       title,
       description,
       createdAt: Date.now(),
@@ -84,6 +84,9 @@ export const postUpload = async (req, res) => {
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
+    const user = await User.findById(_id);
+    user.videos.push(newVideo._id);
+    user.save();
     return res.redirect("/");
   } catch (error) {
     return res.status(400).render("upload", {
