c51097f5de1110a1323a15ab40a01170ccf006f2
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-26 14:59:45 +0900

    8.15
    Conclusions

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,23 +2,23 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
-  fileUrl : { type:String, required : true},
+  fileUrl: { type: String, required: true },
   description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
-    rating: { type: Number, default: 0, required: true },
   },
-  owner : {type:mongoose.Schema.Types.ObjectId, required : true, ref:"User"},
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   // ObjectId는 mongoose에서 제공하는 타입이기에 mongoose.Schema.Types.OjbectId로 사용한다.
   // ref <- User 모델을 참조한다.
 });
 
-videoSchema.static("formatHashtags", function(hashtags){
-  return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
-})
-
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
 
 // middleware는 model이 생성되기 전에 만들어야한다.
 const Video = mongoose.model("Video", videoSchema);
