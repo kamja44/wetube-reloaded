04016292cae2f1ef02669bfb81ce8faea5d69f1f
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-08-29 13:33:35 +0900

    9.2
    Webpack Configuration part Two
    npm i -D babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
+const path = require("path");
+
+// console.log(path.resolve(__dirname, "assets", "js"));
+// path.resolve() <- 입력하는 파트들을 모아서 경로로 만들어줌
+// __dirname <- 파일까지의 절대경로
+// console.log(__dirname);
 module.exports={
     // entry <- 처리하고 싶은 파일 (main.js)
     entry : "./src/client/js/main.js",
+    mode : "development",
     output : {
         filename : "main.js",
         // path <- 파일을 어디 저장할지
-        path : "./assets/js"
-    }
+        path : path.resolve(__dirname, "assets","js"),
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets : [["@babel/preset-env", {targets : "defaults"}]],
+                    },
+                },
+            },
+        ],
+    },
 };
