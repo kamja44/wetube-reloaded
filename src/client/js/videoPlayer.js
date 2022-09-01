d18770c2243025f404c1ac92fe53eb32b9d4d2ea
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-01 14:36:34 +0900

    11.1
    Play Pause Btn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("video player")
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+// handler
+const handlePlayClick = (e) => {
+    if(video.paused){ // paused <- html의 video태그의 기본함수
+        // playBtn.innerText = "Pause";
+        video.play();
+    }else{
+        // playBtn.innerText = "Play";
+        video.pause();
+    }
+}
+const handleMute = (e) => { }
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+// EventListener
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
