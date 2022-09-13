059359e1d048f70384c2e30a7f58b62ea1962473
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-13 15:43:59 +0900

    11.3
    Volume Range

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = Number(volumeValue);
+
 // handler
 const handlePlayClick = (e) => {
     if(video.paused){ // paused <- html의 video태그의 기본함수
@@ -15,17 +18,42 @@ const handlePlayClick = (e) => {
     }
     playBtn.innerText =  video.paused ? "Play" : "Pause";
 }
-const handleMute = (error) => { 
+const handleMute = (event) => { 
     if(video.muted){
         video.muted = false;
     } else{
         video.muted = true;
     }
+    // console.log(vieo.muted);
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
 }
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
+const handleVolumeChange = (event) => {
+    const {
+        target:{
+            value
+        }
+    } = event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+    if(Number(volumeValue) === 0){
+        muteBtn.innerText = "Unmute";
+        video.muted = true;
+    }else{
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+
+    
+}
+
 // EventListener
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange); // input 태그의 value가 바뀔 때(즉, 실시간 반영)
