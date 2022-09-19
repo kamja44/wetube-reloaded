3f875fd86ab5b7e8e8dd4050fdf19c06556bd475
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-09-19 16:30:15 +0900

    13.1
    Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,16 @@
-
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async() => {
     const stream = await navigator.mediaDevices.getUserMedia({
         audio:true,
-        video:true,
+        video:{
+            width:200,
+            height: 200,
+        },
     });
-    console.log(stream);
+    video.srcObject = stream;
+    video.play();
 }
 
 startBtn.addEventListener("click", handleStart);
